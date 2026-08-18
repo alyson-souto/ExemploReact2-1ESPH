47467a9a0279a647304424a5d55ff7d1aa11e5f6
Alyson <alysonartist@gmail.com>
2026-08-18 11:21:34 -0300

    feat: adição dos componentes Aluno e Dados .jsx

---
+++ src/components/Aluno.jsx
@@ -0,0 +1,12 @@
+
+const Aluno = (props) => {
+  return (
+    <>
+    <h5>PROPS</h5>
+      <p>O nome do Aluno é: {props.nome} </p>
+      <p>A idade do Aluno é: {props.idade} </p>
+    </>
+  )
+}
+
+export default Aluno
---
+++ src/components/Dados.jsx
@@ -0,0 +1,13 @@
+
+const Dados = ({usuario, email}) => {
+  return (
+
+    <>
+    <h5>DESESTRUTURAÇÃO</h5>
+      <p>O Nome do usuário é: {usuario}</p>
+      <p>O Email do Usuário é: {email}</p>
+    </>
+  )
+}
+
+export default Dados
